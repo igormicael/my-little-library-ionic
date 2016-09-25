0553201043c7b48de739191db56fe96e0f275df3
igor micael <igor.m.peixoto@gmail.com>
2016-09-25 11:11:24 -0300

    cleaning the app

---
+++ www/js/services.js
@@ -0,0 +1,25 @@
+/**
+ * Created by igor on 25/09/2016.
+ */
+
+'use strict';
+
+//angular.module('confusionApp')
+angular.module('conFusion.services', ['ngResource'])
+  .constant("baseURL","http://localhost:3000/")
+
+   .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+
+    return $resource(baseURL+"leadership/:id");
+
+  }])
+
+  .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+
+    return $resource(baseURL+"feedback/:id");
+
+  }])
+
+;
