933f39b0019cb89790f832807359b0c4a727e96e
igor micael <igor.m.peixoto@gmail.com>
2016-09-26 22:13:00 -0300

    login and logout func

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,16 @@
 angular.module('mll.controllers', [])
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicPlatform) {
+  .controller('AppCtrl', function ($scope, $rootScope, $ionicModal, $timeout, $ionicPlatform, $localStorage, AuthFactory) {
 
     // Form data for the login modal
-    $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = $localStorage.getObject('userinfo','{}');
+    $scope.loggedIn = false;
+
+    if(AuthFactory.isAuthenticated()) {
+      $scope.loggedIn = true;
+      $scope.username = AuthFactory.getUsername();
+    }
 
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -25,13 +32,67 @@ angular.module('mll.controllers', [])
     // Perform the login action when the user submits the login form
     $scope.doLogin = function () {
       console.log('Doing login', $scope.loginData);
+      $localStorage.storeObject('userinfo',$scope.loginData);
 
+      AuthFactory.login($scope.loginData);
+
+      $scope.closeLogin();
+    };
+
+    $scope.logOut = function() {
+      AuthFactory.logout();
+      $scope.loggedIn = false;
+      $scope.username = '';
+    };
+
+    $rootScope.$on('login:Successful', function () {
+      $scope.loggedIn = AuthFactory.isAuthenticated();
+      $scope.username = AuthFactory.getUsername();
+    });
+
+    // REGISTRATION
+
+    $scope.registration = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/register.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.registerform = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeRegister = function () {
+      $scope.registerform.hide();
+    };
+
+    // Open the login modal
+    $scope.register = function () {
+      $scope.registerform.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doRegister = function () {
+      console.log('Doing registration', $scope.registration);
+      $scope.loginData.username = $scope.registration.username;
+      $scope.loginData.password = $scope.registration.password;
+
+      AuthFactory.register($scope.registration);
       // Simulate a login delay. Remove this and replace with your login
       // code if using a login system
       $timeout(function () {
-        $scope.closeLogin();
+        $scope.closeRegister();
       }, 1000);
     };
+
+    $rootScope.$on('registration:Successful', function () {
+      $scope.loggedIn = AuthFactory.isAuthenticated();
+      $scope.username = AuthFactory.getUsername();
+      $localStorage.storeObject('userinfo',$scope.loginData);
+    });
+
+
+
   })
 
   .controller('IndexController', ['$scope', 'baseURL', function ($scope, baseURL) {
--- www/js/services.js
+++ www/js/services.js
@@ -7,9 +7,125 @@
 angular.module('mll.services', ['ngResource'])
   .constant("baseURL", "https://my-little-library.herokuapp.com/")
 
+
+  .factory('AuthFactory', ['$resource', '$http', '$localStorage', '$rootScope', 'baseURL', '$ionicPopup', function ($resource, $http, $localStorage, $rootScope, baseURL, $ionicPopup) {
+
+    var authFac = {};
+    var TOKEN_KEY = 'Token';
+    var isAuthenticated = false;
+    var username = '';
+    var authToken = undefined;
+
+    authFac.login = function (loginData) {
+
+      $resource(baseURL + 'users/login')
+        .save(loginData,
+          function (response) {
+            storeUserCredentials({username: loginData.username, token: response.token});
+            $rootScope.$broadcast('login:Successful');
+            console.log('login:Successful');
+          },
+          function (response) {
+            isAuthenticated = false;
+
+            var message = '<div><p>' + response.data.err.message +
+              '</p><p>' + response.data.err.name + '</p></div>';
+
+            var alertPopup = $ionicPopup.alert({
+              title: '<h4>Login Failed!</h4>',
+              template: message
+            });
+
+            alertPopup.then(function (res) {
+              console.log('Login Failed!');
+            });
+          }
+        )
+    };
+
+    authFac.logout = function () {
+      $resource(baseURL + "users/logout").get(function (response) {
+      });
+      destroyUserCredentials();
+    };
+
+    authFac.register = function (registerData) {
+
+      $resource(baseURL + "users/register")
+        .save(registerData,
+          function (response) {
+            authFac.login({username: registerData.username, password: registerData.password});
+
+            $rootScope.$broadcast('registration:Successful');
+          },
+          function (response) {
+
+            var message = '<div><p>' + response.data.err.message +
+              '</p><p>' + response.data.err.name + '</p></div>';
+
+            var alertPopup = $ionicPopup.alert({
+              title: '<h4>Registration Failed!</h4>',
+              template: message
+            });
+
+            alertPopup.then(function (res) {
+              console.log('Registration Failed!');
+            });
+          }
+        );
+    };
+
+    function loadUserCredentials() {
+      var credentials = $localStorage.getObject(TOKEN_KEY, '{}');
+      if (credentials.username != undefined) {
+        useCredentials(credentials);
+      }
+    }
+
+    function storeUserCredentials(credentials) {
+      $localStorage.storeObject(TOKEN_KEY, credentials);
+      useCredentials(credentials);
+    }
+
+    function useCredentials(credentials) {
+      isAuthenticated = true;
+      username = credentials.username;
+      authToken = credentials.token;
+
+      // Set the token as header for your requests!
+      $http.defaults.headers.common['x-access-token'] = authToken;
+    }
+
+    function destroyUserCredentials() {
+      authToken = undefined;
+      username = '';
+      isAuthenticated = false;
+      $http.defaults.headers.common['x-access-token'] = authToken;
+      $localStorage.remove(TOKEN_KEY);
+    }
+
+    authFac.isAuthenticated = function () {
+      return isAuthenticated;
+    };
+
+    authFac.getUsername = function () {
+      return username;
+    };
+
+    authFac.facebook = function () {
+
+    };
+
+    loadUserCredentials();
+
+    return authFac;
+
+
+  }])
+
   .factory('myBooksFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-    return $resource(baseURL + "mybooks/:id", { id: '@_id' }, {
+    return $resource(baseURL + "mybooks/:id", {id: '@_id'}, {
       'update': {
         method: 'PUT'
       }
@@ -19,12 +135,29 @@ angular.module('mll.services', ['ngResource'])
 
   .factory('booksFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-    return $resource(baseURL + "books/:id", { id: '@_id' }, {
+    return $resource(baseURL + "books/:id", {id: '@_id'}, {
       'update': {
         method: 'PUT'
       }
     });
 
+  }])
+
+  .factory('$localStorage', ['$window', function ($window) {
+    return {
+      store: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key, defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
   }])
 
 ;
